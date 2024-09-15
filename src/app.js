ea3fb0bccafc512b36577cfb4a5e741513becb72
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-09-15 10:36:33 +0530

    divide index file into app so that index file do not messy

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express(); //app object
+
+//middlewares
+app.use(express.json()); //allowing json to read
+app.use(
+    express.urlencoded({
+        extended: true,
+        limit: '16kb',
+    })
+);
+app.use(
+    cors({
+        origin: '*',
+        credentials: true, // This option indicates whether or not the response to the frontend can include cookies
+    })
+);
+
+//application sample
+app.get('/', function (req, res, next) {
+    res.send('Welcome');
+});
+
+//application routes
+import userRouter from './user/user.router.js';
+app.use('/api/v1', userRouter);
+
+export default app;
+
+/*
+app.listen(PORT, () => { // sample for app to run on a particular port
+    console.log(`Server is running at port:: http://localhost:${PORT}`);
+});
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
-import express from 'express';
 import dotenv from 'dotenv';
 import connetDB from './db/index.js';
-import cors from 'cors';
-
-dotenv.config(); //alowing env file across application
-
-const app = express(); //app object
-
-//middlewares
-app.use(express.json()); //allowing json to read
-app.use(
-    express.urlencoded({
-        extended: true,
-        limit: '16kb',
-    })
-);
-app.use(cors({
-    origin: '*',
-    credentials : true // This option indicates whether or not the response to the frontend can include cookies 
-}))
-
-
-//application sample
-app.get('/', function (req, res, next) {
-    res.send('Welcome');
-});
-
-//application routes
-import userRouter from './user/user.router.js';
-app.use('/api/v1', userRouter);
+import app from './app.js';
+dotenv.config(); //alowing .env file across application
 
 const PORT = process.env.PORT ?? 5500; //port
 
+//connection DB and after that running app on port
 connetDB()
     .then(() => {
         app.listen(PORT, () => {
@@ -41,6 +15,22 @@ connetDB()
     .catch((error) => {
         console.log('Connection failed: ' + error);
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 app.listen(PORT, () => { // sample for app to run on a particular port
